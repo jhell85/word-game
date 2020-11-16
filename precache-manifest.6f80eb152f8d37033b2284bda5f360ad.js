d4b7c11aebda664be10af7e3f2bf61dacf97f4bc
jhell85 <jhell85@gmail.com>
2020-11-15 19:04:29 -0800

    Updates

---
+++ precache-manifest.6f80eb152f8d37033b2284bda5f360ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ab70be79490ac98451c647765b5a9b",
+    "url": "/word-game/index.html"
+  },
+  {
+    "revision": "0adaa987642d37ecdaaf",
+    "url": "/word-game/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "a6c1616c60e053fcf998",
+    "url": "/word-game/static/css/main.65cd5c3f.chunk.css"
+  },
+  {
+    "revision": "0adaa987642d37ecdaaf",
+    "url": "/word-game/static/js/2.26f07565.chunk.js"
+  },
+  {
+    "revision": "a6c1616c60e053fcf998",
+    "url": "/word-game/static/js/main.d15b4881.chunk.js"
+  },
+  {
+    "revision": "dddaca0086e4ecece7fc",
+    "url": "/word-game/static/js/runtime-main.d37e031b.js"
+  }
+]);
